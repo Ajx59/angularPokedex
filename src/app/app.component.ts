b5f9204da53a3d6abed6f615a49aa475739365af
Yacine. <mohammedi.bokhari@gmail.com>
2024-04-05 20:28:03 +0200

    first commit pokedex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {POKEMONS} from "./mock-pokemon-list";
+import {Pokemon} from "./pokemon";
+import {CommonModule } from "@angular/common";
+import {BorderCardDirective} from "./border-card.directive";
+import {PokemonTypeColorPipe} from "./pokemon-type-color.pipe";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [RouterOutlet, CommonModule, BorderCardDirective, PokemonTypeColorPipe],
+  templateUrl: 'app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: Pokemon) {
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+
+    if(pokemon){
+      console.log(`Vous aves demandé le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }else{
+     console.log(`Vous avez demande un pokemon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+  }
+
 }
